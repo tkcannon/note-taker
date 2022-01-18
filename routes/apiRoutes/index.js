e917788f5d3ac7d6b4f81ac47f6272ea60e21bc2
tkcannon <taylorkcannon@gmail.com>
2022-01-18 07:17:33 -0700

    added delete route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { v4: uuid } = require('uuid');
 
 const notes = require('../../db/db.json');
-const createNote = require('../../lib/notes.js')
+const { createNote, deleteNote } = require('../../lib/notes.js')
 
 router.get('/notes', (req, res) => {
     res.json(notes);
@@ -13,6 +13,10 @@ router.post('/notes', (req, res) => {
     note.id = uuid();
     createNote(note, notes);
     res.json({ message: 'note created' });
+})
+
+router.delete('/nodes/:id', (req, res) => {
+    id = req.params.id;
 })
 
 module.exports = router;
