f71cc4060e8cab4d8bbc44b5ae45cb11725931e2
tkcannon <taylorkcannon@gmail.com>
2022-01-22 09:49:47 -0700

    added comments

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,24 +1,33 @@
 const router = require('express').Router();
+const res = require('express/lib/response');
 const { getNotes, createNote, deleteNote } = require('../../lib/notes.js');
 
+// Get All Notes
 router.get('/notes', (req, res) => {
     notes = getNotes();
     res.json(notes);
 })
 
+// Create New Note
 router.post('/notes', (req, res) => {
     if (!req.body.title || !req.body.text) {
-        res.json({ message: 'Missing note data' });
+        res.status(404).json({ message: 'Missing note data' });
+        return;
     }
 
     noteCreated = createNote(req.body);
-
     res.json({ note: noteCreated });
 })
 
+// Delete Note By ID
 router.delete('/notes/:id', (req, res) => {
     const id = req.params.id;
     const deletedNote = deleteNote(id);
+
+    if (!deletedNote) {
+        res.status(404).json({ message: 'No note found with that id' });
+        return;
+    }
 
     res.json({ deletedNote: deletedNote });
 })
